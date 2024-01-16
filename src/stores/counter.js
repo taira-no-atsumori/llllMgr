7a73b8c516bff87fc2e79073133205c9b3c41a52
taira-no-atsumori <syun.freedom@gmail.com>
2024-01-16 19:56:57 +0900

    バージョン情報書き換え

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -5,7 +5,7 @@ import { useMusicStore } from './musicList';
 
 export const useStoreCounter = defineStore('store', {
   state: () => ({
-    version: 'ε.14(アーリーアクセス)',
+    version: 'ε.15(アーリーアクセス)',
     dialog: false,
     showModalName: false,
     updateData: false,
