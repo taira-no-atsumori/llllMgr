aef958d4f729217a4bde0b89275fedb9d56daa27
taira-no-atsumori <syun.freedom@gmail.com>
2025-02-19 20:15:12 +0900

    # OTHER - 初回ロード時のページ遷移処理の修正

--- src/App.vue
+++ src/App.vue
@@ -282,7 +282,7 @@ export default {
       localStorage.removeItem('inflow');
 
       for (const listName in this.pageList) {
-        if (this.pageList[listName].url === pageName) {
+        if (this.pageList[listName].url.toLowerCase() === pageName.toLowerCase()) {
           this.pageMove(this.pageList[pageName].name_en);
           return;
         }
